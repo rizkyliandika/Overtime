c45c2a998d155a82d09617d2e0f513587f7cfa04
Akmal Hisqie <akmalhisqie@gmail.com>
2023-12-06 19:31:56 +0700

    Add Filament Resources

---
+++ app/Filament/Resources/UserManagementResource.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\UserManagementResource\Pages;
+use App\Filament\Resources\UserManagementResource\RelationManagers;
+use App\Models\UserManagement;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Filament\Forms\Components\TextInput;
+use Filament\Tables\Columns\TextColumn;
+
+class UserManagementResource extends Resource
+{
+    protected static ?string $model = UserManagement::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('id'),
+                TextColumn::make('username'),
+                TextColumn::make('name'),
+                TextColumn::make('group_name'),
+
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListUserManagement::route('/'),
+            'create' => Pages\CreateUserManagement::route('/create'),
+            'edit' => Pages\EditUserManagement::route('/{record}/edit'),
+        ];
+    }
+}
