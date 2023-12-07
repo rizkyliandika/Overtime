a6905058ad928a015ddc047077528afa3b24beff
akmalhisqie <akmalhisqie@gmail.com>
2023-12-07 21:23:38 +0700

    Update Migrations

--- database/migrations/2023_12_20_225456_create_types_table.php
+++ database/migrations/2023_12_20_225456_create_types_table.php
@@ -16,6 +16,11 @@ return new class extends Migration
             $table->string('name');
             $table->timestamps();
         });
+
+        // Schema::table('user_groups', function (Blueprint $table) {
+        //     $table->foreign('user_id')->references('id')->on('users')
+        //         ->onDelete('cascade')->onUpdate('cascade');
+        // });
     }
 
     /**
