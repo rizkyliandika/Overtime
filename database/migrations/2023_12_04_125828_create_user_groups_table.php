ba5967c35708dc5f9c2e3fdf61a2fce0e2f8ccba
Rizky Liandika <rizkyliandika@gmail.com>
2023-12-09 08:50:51 +0700

    [REFACTOR] add signup,login,and profile screen

--- database/migrations/2023_12_04_125828_create_user_groups_table.php
+++ database/migrations/2023_12_04_125828_create_user_groups_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('user_groups', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
             $table->string('group_name');
             $table->timestamps();
         });
