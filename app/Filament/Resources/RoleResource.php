9fb485d9422bdf1bd4f0ec4b1b3199df643805ea
Rizky Liandika <rizkyliandika@gmail.com>
2023-12-18 02:46:52 +0700

    [CHORES] remove unused import & page

--- app/Filament/Resources/RoleResource.php
+++ app/Filament/Resources/RoleResource.php
@@ -9,13 +9,10 @@ use Filament\Forms\Components\MultiSelect;
 use Filament\Forms\Components\TextInput;
 use Filament\Tables\Columns\TextColumn;
 use Spatie\Permission\Models\Role;
-use Filament\Forms;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\SoftDeletingScope;
 
 class RoleResource extends Resource
 {
--- app/Filament/Resources/UserLogResource/Pages/CreateUserLog.php
+++ app/Filament/Resources/UserLogResource/Pages/CreateUserLog.php
@@ -3,7 +3,6 @@
 namespace App\Filament\Resources\UserLogResource\Pages;
 
 use App\Filament\Resources\UserLogResource;
-use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateUserLog extends CreateRecord
