ba3efe78191af8c5663bf267593a3607770e5d28
Rizky Liandika <rizkyliandika@gmail.com>
2023-12-18 02:46:52 +0700

    [CHORES] remove unused import & page

--- app/Filament/Resources/PermissionResource.php
+++ app/Filament/Resources/PermissionResource.php
@@ -3,8 +3,6 @@
 namespace App\Filament\Resources;
 
 use App\Filament\Resources\PermissionResource\Pages;
-use App\Filament\Resources\PermissionResource\RelationManagers;
-use Filament\Forms;
 use Filament\Forms\Components\Card;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Form;
@@ -12,8 +10,6 @@ use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\SoftDeletingScope;
 use Spatie\Permission\Models\Permission;
 
 class PermissionResource extends Resource
