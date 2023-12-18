55e5d2a9b178ade999a61366db49e2bbc684719a
Rizky Liandika <rizkyliandika@gmail.com>
2023-12-19 03:12:23 +0700

    [CHORES] add nullable()

--- database/migrations/2023_12_17_165052_add_user_id_to_user_logs_table.php
+++ database/migrations/2023_12_17_165052_add_user_id_to_user_logs_table.php
@@ -11,7 +11,7 @@ return new class extends Migration {
     public function up(): void
     {
         Schema::table('user_logs', function (Blueprint $table) {
-            $table->foreignId('user_id')->after('id')->constrained()->cascadeOnDelete();
+            $table->foreignId('user_id')->nullable()->after('id')->constrained()->cascadeOnDelete();
         });
     }
 
