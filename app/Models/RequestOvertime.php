e6f69ff3baaeeb0dd6d413eaf8a8888c31e0d4d1
Rizky Liandika <rizky.liandika@baf.id>
2023-12-20 22:49:36 +0700

    [FEATURES] create model tenant and overtime request

---
+++ app/Models/RequestOvertime.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class RequestOvertime extends Model
+{
+    use HasFactory;
+}
